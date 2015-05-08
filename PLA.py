d3a67a97b7e6a4dc1e5ccb2caf9ca80eda5947b1
jackyliu <jjkka132@gmail.com>
2015-05-08 17:00:54 +0800

    Final answer for HW1_15_to_17

--- PLA.py
+++ PLA.py
@@ -1,99 +1,63 @@
 #2015/03/31 15:00 Jacky Liu
 import numpy as np
+import random
 
-import matplotlib.pyplot as plt
-from matplotlib.animation import ArtistAnimation
-
-def plot(w_vector, data_x, data_y, name):    
-    fig = plt.figure()
-
-    ax = fig.add_subplot(111)
-    ax.axis([-0, 1, -0, 1])
-    #ax.margins(y=.1)
-    
-    
-    for i in range(0, len(data_x)):
-        x = data_x[i][0]
-        y = data_x[i][1]
-        if data_y[i] >= 0:
-            ax.scatter(x, y, color='red', marker='o', s=100)
-        else:
-            ax.scatter(x, y, color='blue', marker='^', s=100)
-    
-    
-    line_x = [-10, 100]
-    line_y = []
-    threshold = w_vector[0]
-    
-    for x_tmp in line_x:
-        try:
-            line_y.append(((-1)*threshold - w_vector[1]*x_tmp) / float(w_vector[2]))
-        except:
-            line_y.append(0)
-    #print(str(line_x) + str(line_y))
-    #plt.plot(line_p1, line_p2, 'k-')
-
-    plot_result = plt.plot(line_x, line_y, 'k-')
-    plt.title('step ' + name)
-    
-    
-    #plt.show()
-    plt.savefig(name + '.png')
-    
-    return plot_result
-    
-
-def h(w_vector, data_x):
-    if np.dot(w_vector[1:], data_x) + w_vector[0] > 0:
-        return 1
-    else:
+def sign(x):
+    if x <= 0:
         return -1
+    return 1 
 
-def update_w_vector(w_vector, data_x, data_y):
-    for i in range(0, len(data_x)):
-        w_vector[i+1] += data_x[i]*data_y
-       
-    
-
-def pla(w_vector, data_x, data_y):
+def pla(w_vector, data_x, data_y, cycleType, eta=1.0):
     flag = 0
-    update_count = 1
+    update_count = 0
+    
+    if cycleType == 'naive':
+        index = range(0, len(data_x))
+    elif cycleType == 'random':
+        index = np.random.permutation(len(data_x))
+    else:
+        return
+
+    
     while flag != 1:
         flag = 1
-        for i in range(0, len(data_x)):
-            if h(w_vector, data_x[i]) != data_y[i]:
-                #print(str(update_count) + ' update')
-                update_w_vector(w_vector, data_x[i], data_y[i])
-                #print('\t' + str(w_vector))
+        for i in index:
+            if sign(np.inner(w_vector, data_x[i])) != data_y[i]:
+                w_vector = w_vector +  eta * data_x[i] * data_y[i]
                 flag = 0
-                #plot(w_vector, data_x, data_y, '{0:02d}'.format(update_count))
                 update_count += 1
+
                 
-    print('Update count = ' + str(update_count))
+    return update_count
             
 
 def main():
     data = []
     
-    f = open("train.dat", "r")
-    for line in f:
-        floats = [float(x) for x in line.split()]
-        data.append(floats)
+    data = np.loadtxt('train.dat', skiprows=0)
     
+    ## Add Y-intercept to first column of data_x
+    data_x = np.ones((len(data),len(data[0])))
+    data_x[:, 1:] = data[:, :-1 ]
+
+    data_y = data[:, -1]
+
+    w_vector = np.zeros(len(data_x[0]))
     
-    #data = [[10, 60, 1], [20, 70, 1], [10, 30, -1], [30, 10, -1]]
-    #data = [[10, 60, 1], [20, 70, 1], [60, 30, -1], [30, 10, -1]]
-    data_x = [i[:4] for i in data]
-    data_y = [i[-1] for i in data]
-    print('Data point:' + str(data_x[0]))
-    print('Type:' + str(data_y[0]))
+    ## HW 15 naive cycle
+    #pla(w_vector, data_x, data_y, 'naive')
     
-    w_vector = [-1, 0, 0, 0, 0]
-    
-    pla(w_vector, data_x, data_y)
-    
-    print(w_vector)
-    
+    ## HW 16 random cycle
+    #count = 0.0
+    #for i in range(2000):
+    #    count += pla(w_vector, data_x, data_y, 'random')    
+    #print(count/2000)
+
+    ## HW 17 random cycle
+    #count = 0.0
+    #for i in range(2000):
+    #    count += pla(w_vector, data_x, data_y, 'random', 0.5)    
+    #print(count/2000)
     
 
 if __name__ == '__main__':
